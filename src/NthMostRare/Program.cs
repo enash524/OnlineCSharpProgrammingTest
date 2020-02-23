aa1945e2d4bb98694e164f045cd966103a124c78
Eric Nash <enash524@gmail.com>
2020-02-22 22:40:22 -0600

    1) Initial Commit

---
+++ src/NthMostRare/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace NthMostRare.ConsoleApp
+{
+    public static class Rare
+    {
+        public static int NthMostRare(int[] elements, int n)
+        {
+            return elements
+                .GroupBy(e => e)
+                .Select(i => new { i.Key, Count = i.Count() })
+                .OrderBy(o => o.Count)
+                .ElementAt(n - 1)
+                .Key;
+        }
+
+        public static void Main(string[] args)
+        {
+            int x = NthMostRare(new int[] { 5, 4, 3, 2, 1, 5, 4, 3, 2, 5, 4, 3, 5, 4, 5 }, 2);
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ src/Platformer.ConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Platformer.ConsoleApp
+{
+    public class Platformer
+    {
+        private readonly List<int> _tiles = new List<int>();
+        private int _position;
+
+        public Platformer(int numberOfTiles, int position)
+        {
+            _position = position;
+
+            for (int i = 0; i < numberOfTiles; i++)
+            {
+                _tiles.Add(i);
+            }
+        }
+
+        public void JumpLeft()
+        {
+            int index = _tiles.IndexOf(_position);
+            int newPosition = _tiles[index - 2];
+            _tiles.Remove(_position);
+            _position = newPosition;
+        }
+
+        public void JumpRight()
+        {
+            int index = _tiles.IndexOf(_position);
+            int newPosition = _tiles[index + 2];
+            _tiles.Remove(_position);
+            _position = newPosition;
+        }
+
+        public int Position()
+        {
+            return _position;
+        }
+
+        public static void Main(string[] args)
+        {
+            Platformer platformer = new Platformer(6, 3);
+            Console.WriteLine(platformer.Position());
+
+            platformer.JumpLeft();
+            Console.WriteLine(platformer.Position());
+
+            platformer.JumpRight();
+            Console.WriteLine(platformer.Position());
+        }
+    }
+}
